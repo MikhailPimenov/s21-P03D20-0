2c027ab29dbdc4ec332dea59ffd669e55cb3152c
Elvia Attack <elviaatt@at-p1.msk.21-school.ru>
2022-02-19 15:35:28 +0300

    develop-serg-mitsuecr/scratch for drawing.c. Nothing works here. Serg, this is for you :)

--- src/drawing/drawing.c
+++ src/drawing/drawing.c
@@ -2,7 +2,7 @@
 
 int draw_field(char **field, int rows, int columns) {
     //  TODO: YOUR CODE IS HERE
-
+    printf("hello from draw_field():\n");
     
 
     return 0;
--- src/graph.c
+++ src/graph.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+#include "drawing/drawing.h"
+#include "translating_calculating/calculating.h"
+#include "translating_calculating/translating.h"
 
 
 int main() {
     printf("Hello from main()\n");
+    print_from_translating();
+
+
+    // const int rows = 25;
+    // const int columns = 80;
+ 
+    // char **example_field = NULL;
+    // *example_field = NULL;
+    // example_field = NULL;
+    printf("Before draw_print():\n");
+
+
+    // draw_field(example_field, rows, columns);
+
+    printf("After draw_print():\n");
+    
+    
 
     return 0;
 }
